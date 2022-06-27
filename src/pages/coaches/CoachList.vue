ddb89f646a013939acf686fc9fa2e1c9f5d67ec6
Barry Anderosn <barryanderson@users.noreply.github.com>
2022-06-27 15:03:23 +0100

    add loading spinner while loading from firebase

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -6,9 +6,14 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button link to="/register" v-if="!isCoach">Register</base-button>
+        <base-button link to="/register" v-if="!isCoach && !isLoading"
+          >Register</base-button
+        >
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -39,6 +44,7 @@ export default {
         backend: true,
         career: true,
       },
+      isLoading: false,
     };
   },
   computed: {
@@ -57,7 +63,7 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
     isCoach() {
       return this.$store.getters['coaches/isCoach'];
@@ -70,8 +76,10 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches');
+    async loadCoaches() {
+      this.isLoading = true;
+      await this.$store.dispatch('coaches/loadCoaches');
+      this.isLoading = false;
     },
   },
 };
