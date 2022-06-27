116de9f72b54c02a3ebb39a00938c58c1360ebff
Barry Anderosn <barryanderson@users.noreply.github.com>
2022-06-27 13:54:49 +0100

    add view of recieved messages

--- src/pages/requests/RequestList.vue
+++ src/pages/requests/RequestList.vue
@@ -1,1 +1,53 @@
-<template>Requests</template>
+<template>
+  <section>
+    <base-card>
+      <header><h2>Requests Recieved</h2></header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="r in recievedRequests"
+          :key="r.id"
+          :email="r.email"
+          :message="r.message"
+        ></request-item>
+      </ul>
+      <h3 v-else>You have no requests yet!</h3>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+export default {
+  components: {
+    RequestItem,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    recievedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
--- src/pages/requests/RequestRegistration.vue
+++ src/pages/requests/RequestRegistration.vue
@@ -37,14 +37,14 @@ export default {
         this.formIsValid = false;
         return;
       }
-      this.$store.dispatch[
-        ('requests/addRequest',
-        {
-          coachId: this.$route.params.id,
-          email: this.email,
-          message: this.message,
-        })
-      ];
+
+      console.log('');
+      this.$store.dispatch('requests/addRequest', {
+        coachId: this.$route.params.id,
+        email: this.email,
+        message: this.message,
+      });
+
       this.$router.replace('/coaches');
     },
   },
